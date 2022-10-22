46234f8917646b0cccdb9a3f73f2038e39b96655
123ZOKzok <user email>
2022-10-22 18:37:10 +0300

    Done

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,12 @@
 # Write your code here
+require 'pry'
+
+def reverse_each_word(sentence)
+  binding.pry
+end
+
+reverse_each_word("Hello there, and how are you?")
+
+def reverse_each_word(sentence)
+    words = sentence.split
+  end
